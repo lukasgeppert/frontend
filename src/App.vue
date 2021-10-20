104e87a1bab7bec7f904cd50e5bc01723347b724
Lukasz Zywluk <lzywluk@casio.com>
2021-10-19 20:03:52 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Home :msg="msg"/>
+  </div>
+</template>
+
+<script>
+import axios      from 'axios'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  data () {
+    return {
+      msg: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://backend.demo.test/api/top-100-links')
+      .then(response => (this.msg = response.data))
+  }
+}
+</script>
